2e4ee305c340ee04bd256a763c6950ac456a6f30
ulan <umanasov24@gmail.com>
2020-05-22 23:24:15 -0500

    added s3

--- sandbox/backend.tf
+++ sandbox/backend.tf
@@ -1,6 +1,6 @@
 #for setenv.sh
 s3_bucket_region  = "eu-west-1"
-s3_bucket         = "t-s3-backend-dev"
+s3_bucket         = "test-company123"
 s3_folder_project = "wordpress"
 s3_folder_name    = "dev"
 s3_tfstate_file   = "infrastructure.tfstate"
