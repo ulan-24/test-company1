8ea4a79015df8d4439530e64612e263967c0d3ab
ulan <umanasov24@gmail.com>
2020-05-22 22:52:59 -0500

    added

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,7 +6,7 @@ cidr            = "10.0.0.0/16"
 project         = "Wordpress"
 environment     = "Test"
 
-azs             = ["us-east-1a","us-east-1b","us-east-1c"]
+azs             = ["eu-west-1a","eu-west-1b","eu-west-1c"]
 public_subnets  = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
 private_subnets = ["10.0.11.0/24","10.0.12.0/24","10.0.13.0/24"]
 }
